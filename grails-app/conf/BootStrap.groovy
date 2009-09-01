ae9633e285d160a354718f9e026b15a0e61c5844
Jori Hardman <jyro@arch.(none)>
2009-09-01 12:30:28 -0400

    Changed bootstrap user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,7 +2,7 @@ import grails.util.GrailsUtil
 
 class BootStrap {
     def init = { servletContext ->
-        new User(login:"administrator", password:"supra.map!".encodeAsHash(), name:"Administrator", role:"admin", email:"jori@u.northwestern.edu").save()
+        new User(login:"admin", password:"supra.map!".encodeAsHash(), name:"Administrator", role:"admin", email:"jori@u.northwestern.edu").save()
     }
 
     def destroy = {
