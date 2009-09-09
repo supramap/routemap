86fe1ed2f038f32fbef6c4d44f965e5fb3a67ea5
Jori Hardman <jyro@arch.localdomain>
2009-09-09 14:22:43 -0400

    Fixed Google Analytics.

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -28,11 +28,3 @@ function toggleOptions(name) {
 	}
     }
 }
-
-/* Google Analytics */
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-try {
-    var pageTracker = _gat._getTracker("UA-10595318-1");
-    pageTracker._trackPageview();
-} catch(err) {}
