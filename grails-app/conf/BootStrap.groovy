95b968ac71f5d7e925413649af7043a6def8f57a
Jori Hardman <jyro@arch.(none)>
2009-11-13 16:22:36 -0600

    Increased the session timeout to 1 hour

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,6 +3,7 @@ import grails.util.GrailsUtil
 class BootStrap {
     def init = { servletContext ->
         new User(login:"admin", password:"supra.map!".encodeAsHash(), name:"Administrator", role:"admin", email:"jori@u.northwestern.edu").save()
+        session.setMaxInactiveInterval(3600) //Increase session timeout to 1 hour
     }
 
     def destroy = {
